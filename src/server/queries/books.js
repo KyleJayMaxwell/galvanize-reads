496328ddeab5cef53bef07d22dd91e94fa919ced
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-28 13:16:36 -0600

    ability to add a new book to the database

--- src/server/queries/books.js
+++ src/server/queries/books.js
@@ -9,6 +9,18 @@ module.exports = {
     .then(function(books) {
       return books;
     });
+  },
+  addBook: function(title, genre, description, cover_url){
+    return books().insert({
+      title: title,
+      genre: genre,
+      description: description,
+      cover_url: cover_url
+    })
+    .then(function(results) {
+      console.log(results);
+      return results;
+    });
   }
 
 };
