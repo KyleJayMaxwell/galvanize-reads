8474479f4187eb6201e760add8986e2d6dae05f2
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-28 10:19:36 -0600

    initing knex

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost:5432/gread',
+    seeds: {
+      directory: './seeds/'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    seeds: {
+      directory: './seeds/'
+    }
+  }
+
+
+};
